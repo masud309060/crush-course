1b3d80fc0761b0ed1c861463238b5597d66ca21e
masud309060 <masud306090@gmail.com>
2020-08-27 08:21:35 +0600

    crush course header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import image from '../../fakedata/header-image.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="header-title">
+                    <h2>Online Crush Course</h2> 
+                    <p>by</p>
+                    <h1>Lutu Putu Web</h1>
+                </div>
+                <div className="header-img">
+                <img src={image} alt=""/>
+                </div>
+            </div>
+            <nav className="navbar">
+                    <a href="/home">Home</a>
+                    <a href="/courses">Courses</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/others">Others</a>
+            </nav>    
+        </div>
+    );
+};
+
+export default Header;
